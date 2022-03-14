c7e7088d27d48255fb74b5ac2f26b16b87a91b47
arvind035 <marvindkumar035@gmail.com>
2022-03-14 17:09:06 +0530

    userdataApi

--- Spin-Pay/app/Http/Controllers/UserController.php
+++ Spin-Pay/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Users;
 use Illuminate\Support\Facades\Hash;
+use App\Models\UserData;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -27,4 +29,35 @@ class UserController extends Controller
         $users->save();
         print_r("inserted successfuly");
     }
+
+    public function userdata(Request $request,$id){
+
+
+            $validator = Validator::make($request->all(), [
+                'address_line' => 'required',
+                'city' => 'required',
+                'state' => 'required',
+                'age' => 'required',
+                'gender' => 'required',
+                'dob' => 'required',
+                'image' => 'required'
+                ]);
+
+                if($validator->fails()) {
+                    $flag = false;
+                    return $data['message']['statusText'] = "Validation Failed ".$validator->errors();
+                    } else {
+                            $user= new UserData();
+                            $user->user_id= $request['id'];
+                            $user->address_line= $request['address_line'];
+                            $user->city= $request['city'];
+                            $user->state= $request['state'];
+                            $user->age= $request['age'];
+                            $user->gender= $request['gender'];
+                            $user->dob= $request['dob'];
+                            $user->image= $request['image'];
+                            $user->save();
+                            return "Success";
+                           }
+       }
 }
