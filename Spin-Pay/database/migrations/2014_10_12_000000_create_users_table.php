2500b5751877612fdb80a652a4a9133b53088a6d
sathwik <sathwik.m@mpokket.com>
2022-03-14 10:39:37 +0530

    store users api created

--- Spin-Pay/database/migrations/2014_10_12_000000_create_users_table.php
+++ Spin-Pay/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integer('id')->primary()->autoincrement();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('phone')->unique();
             $table->string('password');
+            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');;
             $table->rememberToken();
             $table->timestamps();
         });
--- Spin-Pay/routes/web.php
+++ Spin-Pay/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+    
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
 
 
