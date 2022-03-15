2f6880efccd323b8a9a8d2e1d9543943ec76d595
Vishal Sharma <vishalpansar0@gmail.com>
2022-03-15 10:23:59 +0530

    intigrating users api with frontend

--- Spin-Pay/resources/views/register.blade.php
+++ Spin-Pay/resources/views/register.blade.php
@@ -26,45 +26,45 @@
            <div class="row mt-4">
             <div class="col-md-6">
                 <div class="inputDiv">
-                    <input type="text" id="username" placeholder="enter full name" required>
+                    <input type="text" id="username" name="name" placeholder="enter full name" required>
                     <small class="form-text text-muted">name should be as per in aadhar card.</small>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="inputDiv">
-                    <input type="email" placeholder="enter email" required>
+                    <input type="email" id="usermail" placeholder="enter email" required>
                 </div>
             </div>
             </div>
             <div class="row mt-4">
                 <div class="col-md-6">
                     <div class="inputDiv">
-                        <input type="password" placeholder="create a new password" required>
+                        <input type="password" id="userpassword" placeholder="create a new password" required>
                         <small class="form-text text-muted">password should contain at least 8 characters.</small>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="inputDiv">
                        
-                        <input type="tel" placeholder="enter mobile number" required>
+                        <input type="tel" id="userphone" placeholder="enter mobile number" required>
                     </div>
                 </div>
                 </div>
                 <div class="row mt-4">
                     <div class="col-md-6">
                         <div class="inputDiv">
-                            <input type="password" placeholder="confirm your password" required>
+                            <input type="password" id="userpasswordcnf" placeholder="confirm your password" required>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="inputDiv">
-                           <button class="btn capbtn" id="verifyEmailBtn" style="float:right">verify email</button>
+                           <button class="btn capbtn" id="joinSpinpayBtn" style="float:right">join</button>
                            <div class="loader mt-2" id="emailVerLoader" style="display:none;float:right;margin-right:10%;"></div>
                         </div>
                     </div>
                     
                     </div>
-                    <div class="row mt-4">
+                    <div class="row mt-4" style="display:none">
                         <div class="col-md-6">
                             <div class="inputDiv">
                                 <div class="row mt-4">                      
@@ -103,5 +103,77 @@
 </div>
 </div>
 </div>
+
+
+@include('layouts.jsfiles')
+
+<script>
+    let role = 0;
+    $('#lenderRole').on('click',function(){
+        if(role==0){
+            $('#errorDiv').css('display','none');
+        }
+        role = 3;
+        $('#borrowerRole').css('background-color','white');
+        $('#lenderRole').css('background-color','#3498DB');
+    })
+    $('#borrowerRole').on('click',function(){
+        if(role==0){
+            $('#errorDiv').css('display','none');
+        }
+        role = 4;
+        $('#errorDiv').css('display','none');
+        $('#lenderRole').css('background-color','white');
+        $('#borrowerRole').css('background-color','#3498DB');
+    })
+
+    // $('#verifyEmailBtn').on('click',function(){
+    //   $('#verifyEmailBtn').css('display','none');
+    //   $('#emailVerLoader').css('display','block');
+    //   // $('#otpDiv').css('display','block');
+  
+    // })
+ 
+    $(document).ready(function() {
+        function errormsg(str){
+            $('#errorDiv').html(str);
+              $('#errorDiv').css('display','block');
+        }
+      $('#joinSpinpayBtn').click(function() {
+          if(role==0){
+            errormsg('please select a role - lender or borrower');
+          }
+          $("#userphone").val()=="" ? errormsg('mobile number can not be empty') : phoneInput = $("#userphone").val();
+          $("#userpasswordcnf").val()=="" ? errormsg('confirm password can not be empty') : password_confirmationInput = $("#userpasswordcnf").val();
+          $("#userpassword").val()=="" ? errormsg('password can not be empty') : passwordInput = $("#userpassword").val();
+          $("#usermail").val()=="" ? errormsg('email can not be empty') : mailInput = $("#usermail").val();
+          $("#username").val()=="" ? errormsg('name can not be empty') : nameInput = $("#username").val();
+          if(passwordInput!=password_confirmationInput){
+            errormsg('password and confirm password should be matched.');
+          }else{
+                var getData = {
+                    name: nameInput,
+                    email: mailInput,
+                    phone: phoneInput,
+                    password: passwordInput,
+                    password_confirmation: password_confirmationInput,
+                    role_id: role
+                };
+                $.ajax({
+                    url:"/api/store_users",
+                    type:"post",
+                    dataType: "json",
+                    data: getData,
+                    success: function(result) {
+                        console.log(result);
+                    }
+                });
+                
+          }
+          
+
+      });
+    });
+  </script>
 
 @include('layouts.footer')
