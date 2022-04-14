adba4c485d11c565b61381d4ca79b4428ada29b9
sathwik <sathwik.m@mpokket.com>
2022-04-14 12:49:59 +0530

    ShowUsersDetails, DocumentApprove and CheckLoanRequest api's

--- Spin-Pay/app/Http/Controllers/AgentDashboardController.php
+++ Spin-Pay/app/Http/Controllers/AgentDashboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Users;
 use App\Models\UserData;
+Use App\Models\UserDocument;
+use App\Models\Requests;
 use DB;
 use Illuminate\Database\QueryException;
 
@@ -43,5 +45,83 @@ class AgentDashboardController extends Controller
             ]);
         }
         
+    }
+
+    public function ShowUsersDetails(Request $req){
+        try{
+            $basicInfo = Users::where('users.id',$req['id'])
+                    ->leftjoin('user_datas', 'user_datas.user_id', '=', 'users.id')
+                    ->leftjoin('credit_details', 'credit_details.user_id', '=', 'users.id')
+                    ->select('users.name','users.email','users.phone','users.role_id','user_datas.age', 'user_datas.gender',
+                    'user_datas.dob','user_datas.image','user_datas.address_line','user_datas.city','user_datas.state'
+                    ,'user_datas.pincode','credit_details.credit_limit','credit_details.credit_score')
+                    ->first();
+
+        $docs = Users::where('users.id',$req['id'])
+                        ->leftjoin('user_documents', 'user_documents.user_id', '=', 'users.id')
+                        ->select('user_documents.master_document_id','user_documents.document_number','user_documents.document_image','user_documents.is_verified')
+                        ->get();
+        return response()->json([
+            $basicInfo,
+            $docs   
+        ]);
+        }
+        catch(QueryException $e){
+            return response()->json([
+                'message' => 'Internal Server Error',
+                "status" => 500
+            ]);
+        } 
+    }
+
+    public function DocAprv(Request $req){
+        try{
+            $query = UserDocument::where('user_id', $req['user_id'])
+                          ->where('master_document_id',$req['master_document_id'])
+                          ->update(['is_verified' => 1]);
+            if($query)
+                return "success";
+            else
+                return "failed";
+        }
+        catch(QueryException $e){
+            return response()->json([
+                'message' => 'Internal Server Error',
+                "status" => 500
+            ]);
+        } 
+
+    }
+
+    public function CheckLoanRequest(Request $req){
+        try{
+            $query = Requests::where('user_id',$req['id'])
+                    ->wherebetween(DB::raw('DATE(`created_at`)'), [$req['st'], $req['en']])
+                    ->select('amount', 'status', 'tenure', 'created_at', 'updated_at');
+    
+    
+            if($req['status'] == 'all'){
+                $query = $query->get();
+                return $query;
+            }
+            else if($req['status'] == 'pending'){
+                $query = $query->where('status', $req['status'])->get();
+                return $query;
+            }
+            else if($req['status'] == 'approved'){
+                $query = $query->where('status', $req['status'])->get();
+                return $query;
+            }
+            else{
+                $query = $query->where('status', $req['status'])->get();
+                return $query;
+            }
+        }
+        catch(QueryException $e){
+            return response()->json([
+                'message' => 'Internal Server Error',
+                "status" => 500
+            ]);
+        }
     }
 }
