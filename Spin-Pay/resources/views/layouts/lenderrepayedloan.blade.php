728de363c6625bfb1e729ea680ba4796d68e02a7
abhishek-mpokket <abhishek.borana@mpokket.com>
2022-05-09 17:35:36 +0530

    issue solved

--- Spin-Pay/resources/views/layouts/lenderrepayedloan.blade.php
+++ Spin-Pay/resources/views/layouts/lenderrepayedloan.blade.php
@@ -6,10 +6,10 @@
         {{-- <h1>Loan Repayed Alert Aganist SPINPAY00E{{$loanid}}</h1> --}}
     </div>
     <div>
-        <h3>Transaction ID {{$tid}} </h3>
+        <h3>Transaction ID SPINPAY00E{{$tid}} </h3>
     </div>
     <div>
         {{-- <h2>{{$message}}</h2> --}}
-        <h3>Loan Amount <span style="color:red">{{$amount}}</span></h3>
+        <h3>Loan Amount :  &#8377;<span style="color:red">{{$amount}}</span></h3>
     </div>
 </center>
